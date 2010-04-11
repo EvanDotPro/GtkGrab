58ad57337b26e4623b389e3efcb49e472cc146fa
Evan Coury <me@evancoury.com>
2010-04-11 12:54:56 -0700

    fancy comment block in python file now

--- screenshot.py
+++ screenshot.py
@@ -1,6 +1,22 @@
 #!/usr/bin/python
-# Copyright: Evan Coury <me@evancoury.com>
-# License: GNU GPL, version 3; http://www.gnu.org/licenses/gpl-3.0.txt
+# This file is part of GtkGrab.
+#
+# GtkGrab is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; version 3 of the License.
+#
+# GtkGrab is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with GtkGrab.  If not, see <http://www.gnu.org/licenses/>.
+#
+# @category   GtkGrab
+# @license    http://www.gnu.org/licenses/gpl-3.0.txt GPL
+# @copyright  Copyright 2010 Evan Coury (http://www.Evan.pro/)
+# @package    Client
 
 from pynotify import *
 import sys, urllib2, urllib, os, gtk, base64
